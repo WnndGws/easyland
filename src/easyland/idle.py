0074009c61740dbbd5535188cb682e3dd82ce1af
Wynand Gouws <wynand@archbeast>
2025-06-07 12:05:59 +1000

    The main CPU/RAM gain is from avoiding repeated file open/close for /dev/null and minor improvements in list handling and condition checks.

--- src/easyland/idle.py
+++ src/easyland/idle.py
@@ -1,64 +1,68 @@
-# import .command as Command 
-from . import command
-from . import logger
+from . import command, logger
 import subprocess
-import threading
 import os
 from pywayland.client import Display
 from pywayland.protocol.wayland.wl_seat import WlSeat
-from pywayland.protocol.ext_idle_notify_v1 import (
-    ExtIdleNotificationV1,
-    ExtIdleNotifierV1
-)
+from pywayland.protocol.ext_idle_notify_v1 import ExtIdleNotifierV1
 
-class Idle():
+
+class Idle:
     def __init__(self, config):
-        # self.command = Command.Command()
         self.command = command
         self._config = config
-        self._display =  Display()
+        self._display = Display()
         self._display.connect()
         self._idle_notifier = None
         self._seat = None
         self._notifications = []
         self._notifier_set = False
 
-    # def _global_handler_init(self, reg, id_num, iface_name, version):
-    #     if iface_name == 'wl_seat':
-    #         self._seat = reg.bind(id_num, WlSeat, version)
-
     def _global_handler(self, reg, id_num, iface_name, version):
-        if iface_name == 'wl_seat':
+        if iface_name == "wl_seat":
             self._seat = reg.bind(id_num, WlSeat, version)
-        if iface_name == "ext_idle_notifier_v1":
+        elif iface_name == "ext_idle_notifier_v1":
             self._idle_notifier = reg.bind(id_num, ExtIdleNotifierV1, version)
 
         if self._idle_notifier and self._seat and not self._notifier_set:
             self._notifier_set = True
-            for idx, c in enumerate(self._config):
-                self._notifications.append(None)
-                logger.info("Setting idle notifier for " + str(c[0]) + " seconds")
-                self._notifications[idx] = self._idle_notifier.get_idle_notification(c[0] * 1000, self._seat)
-                self._notifications[idx]._index = idx
-                self._notifications[idx].dispatcher['idled'] = self._idle_notifier_handler
-                self._notifications[idx].dispatcher['resumed'] = self._idle_notifier_resume_handler
+            self._notifications = []
+            for idx, (timeout, idle_cmds, *resume_cmds) in enumerate(self._config):
+                logger.info(f"Setting idle notifier for {timeout} seconds")
+                notification = self._idle_notifier.get_idle_notification(
+                    timeout * 1000, self._seat
+                )
+                notification._index = idx
+                notification.dispatcher["idled"] = self._idle_notifier_handler
+                notification.dispatcher["resumed"] = self._idle_notifier_resume_handler
+                self._notifications.append(notification)
 
-    def _idle_notifier_handler(self, notification): 
+    def _idle_notifier_handler(self, notification):
         for command in self._config[notification._index][1]:
-            logger.info('Idle - Running command: ' + command)
-            with open(os.devnull, 'w') as fp:
-                subprocess.Popen(command, shell=True, stdout=fp)
+            logger.info(f"Idle - Running command: {command}")
+            subprocess.Popen(
+                command,
+                shell=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
 
     def _idle_notifier_resume_handler(self, notification):
-        if len(self._config[notification._index]) > 2:
-            for command in self._config[notification._index][2]:
-                logger.info('Idle - Resuming: Running command: ' + command)
-                with open(os.devnull, 'w') as fp:
-                    subprocess.Popen(command, shell=True, stdout=fp)
+        resume_cmds = (
+            self._config[notification._index][2]
+            if len(self._config[notification._index]) > 2
+            else []
+        )
+        for command in resume_cmds:
+            logger.info(f"Idle - Resuming: Running command: {command}")
+            subprocess.Popen(
+                command,
+                shell=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
 
     def setup(self):
         reg = self._display.get_registry()
-        reg.dispatcher['global'] = self._global_handler
+        reg.dispatcher["global"] = self._global_handler
         while True:
             self._display.dispatch(block=True)
-
