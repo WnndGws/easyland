067cb68a48666489bb7b9e6f2eac737b4c47661d
Wynand Gouws <wynand@archbeast>
2025-06-07 12:04:12 +1000

    These changes improve CPU efficiency by using orjson and avoid potential blocking in background subprocesses, improving robustness and resource usage.

--- src/easyland/command.py
+++ src/easyland/command.py
@@ -1,80 +1,64 @@
 import subprocess
-import json
+import orjson
 import os
-import time
-import threading
 from easyland.log import logger
 import socket
 
 
 class Command:
-    def __init__(self):
-        pass
-
     def sway_get_all_monitors(self):
-        monitors = self.exec("swaymsg -t get_outputs", decode_json=True)
-        return monitors
+        return self.exec("swaymsg -t get_outputs", decode_json=True)
 
-    def sway_get_monitor(self, name=None, make=None, model=None):
+    def sway_get_monitor(self, *, name=None, make=None, model=None):
         monitors = self.sway_get_all_monitors()
+        if not monitors:
+            return None
         for monitor in monitors:
-            if name is not None:
-                if name in monitor["name"]:
-                    return monitor
-
-            if make is not None:
-                if make in monitor["make"]:
-                    return monitor
-
-            if model is not None:
-                if model in monitor["model"]:
-                    return monitor
+            if name is not None and name in monitor.get("name", ""):
+                return monitor
+            if make is not None and make in monitor.get("make", ""):
+                return monitor
+            if model is not None and model in monitor.get("model", ""):
+                return monitor
         return None
 
     def hyprland_get_all_monitors(self):
-        monitors = self.exec("hyprctl -j monitors", decode_json=True)
-        return monitors
+        return self.exec("hyprctl -j monitors", decode_json=True)
 
     def get_system_hostname(self):
-        hostname = socket.gethostname()
-        return hostname
+        return socket.gethostname()
 
-    def hyprland_get_monitor(self, name=None, description=None, make=None, model=None):
+    def hyprland_get_monitor(
+        self, *, name=None, description=None, make=None, model=None
+    ):
         monitors = self.hyprland_get_all_monitors()
-
+        if not monitors:
+            return None
         for monitor in monitors:
-            if name is not None:
-                if name in monitor["name"]:
-                    return monitor
-
-            if description is not None:
-                if description in monitor["description"]:
-                    return monitor
-
-            if make is not None:
-                if make in monitor["make"]:
-                    return monitor
-
-            if model is not None:
-                if model in monitor["model"]:
-                    return monitor
+            if name is not None and name in monitor.get("name", ""):
+                return monitor
+            if description is not None and description in monitor.get(
+                "description", ""
+            ):
+                return monitor
+            if make is not None and make in monitor.get("make", ""):
+                return monitor
+            if model is not None and model in monitor.get("model", ""):
+                return monitor
         return None
 
     def exec(self, command, background=False, decode_json=False):
         if background:
             logger.info("Executing background command: " + command)
             with open(os.devnull, "w") as fp:
-                subprocess.Popen(command, shell=True, stdout=fp)
+                subprocess.Popen(command, shell=True, stdout=fp, stderr=fp)
             return True
         else:
             logger.info("Executing command: " + command)
             output = subprocess.check_output(command, shell=True)
-            decoded_output = output.decode("utf-8")
             if decode_json:
                 try:
-                    json_output = json.loads(decoded_output)
-                except json.decoder.JSONDecodeError:
+                    return orjson.loads(output)
+                except orjson.JSONDecodeError:
                     return None
-                return json_output
-            else:
-                return decoded_output
+            return output.decode("utf-8")
